7fed5498bc7f7fc01ebc308e230ad2b17fd45aca
Alex Sweetall <imsmastermsk@gmail.com>
2022-04-12 18:21:40 +0300

    add system python bench in pyperf suite

--- pyperfsuite.sh
+++ pyperfsuite.sh
@@ -1,4 +1,8 @@
 
+pyenv global system
+python3 -m pip install pyperformance
+pyperformance run -r -o py30810-sys.json  # --debug-single-value -b nbody
+
 pyenv global 3.7.9
 python3 -m pip install pyperformance
 pyperformance run -r -o py30709.json  # --debug-single-value -b nbody
@@ -19,6 +23,7 @@ pyenv global 3.11.0a6
 python3 -m pip install pyperformance
 pyperformance run -r -o py311a6.json  # --debug-single-value -b nbody
 
+pyenv global 3.10.4
 
 python3 -m pyperf compare_to py30709.json py30810.json py30912.json py31004.json py311a6.json --table > pyperf.txt
 
