df60389ee62444223b59704d4a5b85150b3b18ac
Alex Sweetall <46340861+okeangel@users.noreply.github.com>
2022-04-14 11:49:11 +0300

    Update pyperfsuite.sh

--- pyperfsuite.sh
+++ pyperfsuite.sh
@@ -1,34 +1,46 @@
 
 sudo apt update
-sudo apt upgrade
-sudo apt install python3-pip
+sudo apt upgrade -y
+pyenv update || cd ~/.pyenv && git pull && cd
+
+# Install some Python versions
+pyenv install --skip-existing 3.7.9
+pyenv install --skip-existing 3.8.10
+pyenv install --skip-existing 3.9.12
+pyenv install --skip-existing 3.10.4
+pyenv install --skip-existing 3.11.0a6
 
 pyenv global system
-python3 -m pip install pyperformance
-pyperformance run -r -o py30810-sys.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip || sudo apt install python3-pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperfsys.json  # --debug-single-value -b nbody
 
 pyenv global 3.7.9
-python3 -m pip install pyperformance
-pyperformance run -r -o py30709.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperf30709.json  # --debug-single-value -b nbody
 
 pyenv global 3.8.10
-python3 -m pip install pyperformance
-pyperformance run -r -o py30810.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperf30810.json  # --debug-single-value -b nbody
 
 pyenv global 3.9.12
-python3 -m pip install pyperformance
-pyperformance run -r -o py30912.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperf30912.json  # --debug-single-value -b nbody
 
 pyenv global 3.10.4
-python3 -m pip install pyperformance
-pyperformance run -r -o py31004.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperf31004.json  # --debug-single-value -b nbody
 
 pyenv global 3.11.0a6
-python3 -m pip install pyperformance
-pyperformance run -r -o py311a6.json  # --debug-single-value -b nbody
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pyperformance || python3 -m pip install pyperformance
+python3 -m pyperformance run -r -o pyperf311a6.json  # --debug-single-value -b nbody
 
-pyenv global 3.10.4
+pyenv global system
 
-python3 -m pyperf compare_to py30709.json py30810.json py30810-sys.json py30912.json py31004.json py311a6.json --table > pyperf.txt
-
-pyperformance compare --csv pyperf.csv py30709.json py311a6.json
+python3 -m pyperf compare_to pyperf30709.json pyperf30810.json pyperf30912.json pyperf31004.json pyperf311a6.json pyperfsys.json --table > pyperftable.txt
+pyperformance compare --csv pyperfdata.csv py30709.json py311a6.json
